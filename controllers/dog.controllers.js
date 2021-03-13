712b036208e73ea3fd0a56f59794e7fd6d3c75d7
Santiago Contreras <santiagoc213@gmail.com>
2021-03-13 12:58:44 +0100

    modified dog controllers and message moedl

--- controllers/dog.controllers.js
+++ controllers/dog.controllers.js
@@ -1,4 +1,5 @@
 const Dog = require("../model/dog.model");
+const User = require("../model/user.model");
 
 exports.getAllDogs = async (req, res) => {
   const dogs = await Dog.find();
@@ -12,19 +13,28 @@ exports.getOneDog = async (req, res) => {
 };
 
 exports.createDog = async (req, res) => {
-  const { name, age, breed } = req.body;
-  const hasMissingCredentials = !name || !age || !breed;
+  const { name, age, breed, gender, description } = req.body;
+  const { userId } = req.session;
+  const hasMissingCredentials =
+    !name || !age || !breed || !gender || !description;
   if (hasMissingCredentials) {
     return res.status(400).json({ message: "missing credentials" });
   }
-  const dog = await Dog.create(req.body);
-  res.status(200).json(dog);
+  const newDog = await Dog.create(req.body);
+  const userCreateNewDog = await User.findByIdAndUpdate(
+    userId,
+    { $push: { ownedDogs: newDog._id } },
+    { new: true }
+  );
+  res.status(200).json(newDog);
 };
 
 exports.updateDog = async (req, res) => {
   const { dogId } = req.params;
-  const dog = await Dog.findByIdAndUpdate(dogId, req.body);
-  res.status(200).json(dog);
+  const updatedDog = await Dog.findByIdAndUpdate(dogId, req.body, {
+    new: true,
+  });
+  res.status(200).json(updatedDog);
 };
 
 exports.deleteDog = async (req, res) => {
--- model/message.model.js
+++ model/message.model.js
@@ -5,6 +5,7 @@ const MessageSchema = new mongoose.Schema(
     author: { type: String, required: true },
     message: { type: String, required: true },
     dog: { type: mongoose.Schema.Types.ObjectId, ref: "Dog" },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
